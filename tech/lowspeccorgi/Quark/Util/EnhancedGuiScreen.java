33071db22c9e25de1d5595f1d0bf047414c75754
Basilicous <59167551+LowSpecCorgi@users.noreply.github.com>
2021-02-03 13:26:54 +0000

    Ily :heart:

---
+++ tech/lowspeccorgi/Quark/Util/EnhancedGuiScreen.java
@@ -0,0 +1,37 @@
+package tech.lowspeccorgi.Quark.Util;
+
+
+import net.minecraft.client.gui.GuiScreen;
+import tech.lowspeccorgi.Quark.Elements.ElementManager;
+
+import java.io.IOException;
+
+/**
+ * Thanks Matthew <3
+ */
+public class EnhancedGuiScreen extends GuiScreen {
+
+    ElementManager em = new ElementManager();
+
+    @Override
+    public void initGui() {
+        em.clear();
+    }
+
+    @Override
+    public void drawScreen(int mouseX, int mouseY, float partialTicks) {
+        em.onRender(mouseX, mouseY, partialTicks);
+    }
+
+    @Override
+    protected void keyTyped(char typedChar, int keyCode) throws IOException {
+        em.onKeyTyped(typedChar, keyCode);
+        super.keyTyped(typedChar, keyCode);
+    }
+
+    @Override
+    protected void mouseClicked(int mouseX, int mouseY, int mouseButton) throws IOException {
+        em.onMouseClicked(mouseX, mouseY, mouseButton);
+        super.mouseClicked(mouseX, mouseY, mouseButton);
+    }
+}
